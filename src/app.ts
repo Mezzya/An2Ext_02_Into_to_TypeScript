db1c6692812c76b56f717dffb72b087592df3291
mezz <andrey@cryptex.com.ua>
2018-10-04 01:25:01 +0300

    Task #1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,35 @@
 showHello('greeting', 'TypeScript');
 
+
 function showHello(divName: string, name: string) {
     const elt = document.getElementById(divName);
-    elt.innerText = `Hello from ${name}`;
+    elt.innerText = `Hello from ${name}`;   
 }
+
+// Task #1.1
+function getAllBooks() {
+    let books = [
+        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
+        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
+        { title: 'CSS Secrets', author: 'Lea Verou', available: true },
+        { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli',
+        available: true }
+    ]
+
+    return books;
+}
+
+// Task #1.2
+function logFirstAvailable(books: { title: string, author: string, available: boolean }[]) : void {
+    
+    console.log('Length of books : '+books.length);
+    
+    for (const book of books) {
+        if (book!=null){
+            console.log('Title of first book : '+book.title);
+            break;
+        }
+    }
+}
+// Task #1.3
+logFirstAvailable(getAllBooks());
