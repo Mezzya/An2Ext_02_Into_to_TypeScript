68b7d46c94b46650e9ea065af837d4af87e43810
mezz <andrey@cryptex.com.ua>
2018-10-04 01:58:28 +0300

    Task #2

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+enum Category  {"JavaScript", "CSS", "HTML", "TypeScript", "Angular"}; 
+
 showHello('greeting', 'TypeScript');
 
 
@@ -9,11 +11,11 @@ function showHello(divName: string, name: string) {
 // Task #1.1
 function getAllBooks() {
     let books = [
-        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
-        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
-        { title: 'CSS Secrets', author: 'Lea Verou', available: true },
+        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, Category: Category.JavaScript},
+        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, Category: Category.JavaScript },
+        { title: 'CSS Secrets', author: 'Lea Verou', available: true, Category: Category.CSS },
         { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli',
-        available: true }
+        available: true, Category: Category.JavaScript }
     ]
 
     return books;
@@ -33,3 +35,26 @@ function logFirstAvailable(books: { title: string, author: string, available: bo
 }
 // Task #1.3
 logFirstAvailable(getAllBooks());
+
+// Task #2.3
+function getBookTitlesByCategory(category : Category){
+
+    let jsBooks: Array<String> = new Array;
+
+    for (const book of getAllBooks()) {
+        
+        if (book.Category == category){
+            jsBooks.push(book.title);
+        }
+    }
+
+    return jsBooks;
+}
+
+// Task #2.4
+function logBookTitles(books: Array<String>): void{
+
+    books.forEach(element => console.log(element));
+}
+
+logBookTitles(getBookTitlesByCategory(Category.JavaScript));
